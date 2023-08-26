4fbbd23a3b1e0a4dfa2494d6e91de9fa1f3b96e5
enes1004 <karabacakenes@gmail.com>
2023-08-27 02:05:06 +0900

    add api inside nextjs

--- nextjs/config/laravel_api.ts
+++ nextjs/config/laravel_api.ts
@@ -1,1 +1,2 @@
-export const LARAVEL_API = process.env.LARAVEL_API ?? 'http://localhost:8000';
+const USE_APP_ROUTER_API = true;
+export const LARAVEL_API = USE_APP_ROUTER_API ? 'http://localhost:3000' :process.env.LARAVEL_API ?? 'http://localhost:8000';

405c11d14f2b8e0e725054c864a8ab41fd40de92
enes1004 <karabacakenes@gmail.com>
2023-08-27 02:24:59 +0900

    add types

--- nextjs/app/api/dummy_auth/post/[id]/route.ts
+++ nextjs/app/api/dummy_auth/post/[id]/route.ts
@@ -6,3 +6,8 @@ export async function GET(
   ) {
      return NextResponse.json(id<10?{ok:false,reason:"old content only viewable to admin"}:{ok:true});
 }
+
+export type PostAuthData={
+  ok:boolean,
+  reason?:string
+}
